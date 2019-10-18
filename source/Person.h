128e2cc869ab2a47d49b5865ee6ed8874ce886ae
kefsaj <kefsaj@gmail.com>
2019-10-18 18:06:15 -0400

    Initial commit

---
+++ source/Person.h
@@ -0,0 +1,42 @@
+/**
+ Author: Kefin Sajan
+ Date:   10/18/2019
+ */
+
+#ifndef PERSON_H
+#define PERSON_H
+
+#include <string>
+#include "Date.h"
+
+class Person {
+private:
+    std::string IDNum;
+    std::string FNAME;
+    std::string LNAME;
+    Date DOB;
+    bool SEX;
+    
+public:
+    /* Constructors */
+    Person() = default;
+    Person(const std::string& ID, const std::string& FN, const std::string& LN, const Date& D, const bool S);
+    
+    /* Getters */
+    Date getDOB() const;
+    unsigned char getAGE() const;
+    std::string getFNAME() const;
+    std::string getLNAME() const;
+    std::string getIDNum() const;
+    bool getSEX() const;
+    
+    /* Setters */
+    void setDOB(Date const &D);
+    void setFNAME(string const &FN);
+    void setLNAME(string const &LN);
+    void setIDNum(string const &ID);
+    void setSEX(const bool S);
+};
+
+#endif /* PERSON_H */
+
