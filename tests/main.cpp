dac09da116d6e4e93ea16da37c776f7105a8e1b8
kefsaj <KSAj16@KSAj16M-5.local>
2019-10-19 19:58:07 -0400

    Added unit tests for Dialogue Line class

--- tests/main.cpp
+++ tests/main.cpp
@@ -5,12 +5,14 @@ Date:   10/18/2019
 */
 
 #include "PersonTests.h"
+#include "DialogueLineTests.h"
 #include <vector>
 
 int main()
 {
     std::vector<Testable *> unitTests {
         new PersonTests("PersonTests"),
+        new DialogueLineTests("DialogueLineTests"),
     };
     
     for(auto& unitTest : unitTests)
