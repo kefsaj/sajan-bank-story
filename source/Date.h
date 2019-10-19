f9d0f7f058371e20e60977f291da20571ade1d84
Bryan Morfe <bryanmorfe@gmail.com>
2019-10-19 17:14:44 -0400

    Modified Date class interface and fixed a few typos

--- source/Date.h
+++ source/Date.h
@@ -2,25 +2,37 @@
 #ifndef DATE_H
 #define DATE_H
 
-#include <ctime>
+#include <stdint.h>
 
 class Date
 {
-    
 private:
-    int DAY;
-    int MONTH;
-    int YEAR;
+    uint8_t DAY;
+    uint8_t MONTH;
+    uint8_t YEAR;
     
 public:
-    Date()= default;
-    Date(const int& D, const int& M, const int& Y);
-    now();
+    Date() = default;
+    Date(uint8_t D, uint8_t M, uint8_t Y);
     
-    /* Getters*/
-    Date getDate() const;
+    /* Getters */
+    uint8_t getDAY() const;
+    uint8_t getMONTH() const;
+    uint8_t getYEAR() const;
     
-    /* Setters*/
-    void setDate(const int& DAY, const int& MONTH, const int& YEAR);
+    /* Setters */
+    void setDAY(uint8_t D);
+    void setMONTH(uint8_t M);
+    void setYEAR(uint8_t Y);
+    void setDate(uint8_t D, uint8_t M, uint8_t Y);
     
-}
+    /* Operators */
+    Date& operator+(const Date& other);
+    Date& operator-(const Date& other);
+    
+    /* Static members */
+    static Date now();
+    
+};
+
+#endif /* DATE_H */
