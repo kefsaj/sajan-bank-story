58c90f7642c5bb01153a55e42a7dbd789031a985
kefsaj <KSAj16@KSAj16M-5.local>
2019-10-19 20:37:14 -0400

    Made necessary code formating

--- tests/DialogueLineTests.cpp
+++ tests/DialogueLineTests.cpp
@@ -12,19 +12,19 @@ DialogueLineTests::DialogueLineTests(const std::string& n): Testable(n) {}
 void DialogueLineTests::build()
 {
     tests.push([](void) -> TestResult {
-        DialogueLine D1("Hello! ", Person());
-        std::string expected = "Hello! ";
+        DialogueLine D1("Hello!", Person());
+        std::string expected = "Hello!";
         std::string result = D1.getLineToSay();
-        bool passed = ( expected == result );
+        bool passed = (expected == result);
         
         return TestResult(expected, result, passed);
     });
     
     tests.push([](void) -> TestResult {
-        DialogueLine D1("", Person("11123", "", "", Date::now(), true));
+        DialogueLine D1("", Person("11123","","", Date::now(), true));
         std::string expected = "11123";
         std::string result = D1.getPerson().getIDNum();
-        bool passed = ( expected == result );
+        bool passed = (expected == result);
         
         return TestResult(expected, result, passed);
     });
