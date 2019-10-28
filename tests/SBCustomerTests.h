b2acdc5cce4b17097d2b56ecdac6f9ae29153369
kefsaj <KSAj16@KSAj16M-5.local>
2019-10-27 22:33:38 -0400

    Files touched

---
+++ tests/SBCustomerTests.h
@@ -0,0 +1,16 @@
+/*
+Written By Kefin Sajan
+Date: 10/27/19
+*/
+
+#ifndef SB_CUSTOMER_TEST_H
+#define SB_CUSTOMER_TEST_H
+
+#include "SBAccount.h"
+#include "SBCustomer.h"
+
+
+
+
+#endif /* SB_CUSTOMER_TEST_H */
+
---
+++ tests/SBManagerTests.h
@@ -0,0 +1,16 @@
+/*
+Written By Kefin Sajan
+Date: 10/27/19
+*/
+
+#ifndef SB_MANAGER_TEST_H
+#define SB_MANAGER_TEST_H
+
+#include "SBAccount.h"
+#include "SBCustomer.h"
+
+
+
+
+#endif /* SB_MANAGER_TEST_H */
+
